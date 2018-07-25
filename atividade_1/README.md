2e55fbdb06235952affc35b78a026ce275cb9864
Vitor <vytorfelix2@gmail.com>
2018-07-25 19:14:21 -0300

    versão final para entrega

---
+++ atividade_1/README.md
@@ -0,0 +1,11 @@
+# Ordenação por Comparação 1
+
+## Implementação dos algoritmos:
+- Selection Sort: ***selectionSort.py***
+- Inserction Sort: ***inserctionSort.py***
+
+Os algoritmos foram implementados em python e podem ser executados nas versões 2.7 e 3.5.
+
+Um dos arquivos da pasta ***instancias-num*** deve ser passado como argumento, para que este seja organizado. O resultado é exibido no terminal.
+
+O tempo decorrido da execução do algoritmo é dado ao final da exibição dos números organizados. 
--- atividade_1/selectionSort.py
+++ atividade_1/selectionSort.py
@@ -1,29 +1,32 @@
 import argparse
-import time
+import time 
 
-def insertionSort(A):
+def selectionSort(A, a_size):
 	"""
-		Sort an array through the Insertion Sort Algorithm
+		Sort an array through the Selection Sort Algorithm
 		return algorithm's elapsed time
 	"""
 	start_time = time.time()
 
-	for j in range(1, len(A)):
-		key = A[j]
-		i 	= j - 1
-		
-		while i > -1 and A[i] > key:	
-			A[i + 1] = A[i]				
-			i = i - 1
-		
-		A[i + 1] = key
+	for i in range(a_size):
+		min = i
+
+		for j in range(i+1, a_size):
+			
+			if A[j] < A[min]:
+				min = j
+
+		if A[i] != A[min]:
+			aux = A[i]
+			A[i] = A[min]
+			A[min] = aux	
 
 	return time.time() - start_time
 
 if __name__ == "__main__":
-	
-	unsorted_array = []
 
+	unsorted_array = []
+	
 	parser 	= argparse.ArgumentParser(description='Sort an unsorted list of numbers')
 	parser.add_argument("--file", "-f", type=str, required=True)
 	args 	= parser.parse_args()
@@ -33,9 +36,8 @@ if __name__ == "__main__":
 		number = int(line.strip('\r\n'))
 		unsorted_array.append(number)
 
-	elapsed_time = insertionSort(unsorted_array)		
-	
+	elapsed_time = selectionSort(unsorted_array, len(unsorted_array))		
 	for number in unsorted_array:
 		print(number) 
-
-	print("\nArray sorted in {} seconds".format(elapsed_time))
+		
+	print("\n Array sorted in {} seconds".format(elapsed_time))
